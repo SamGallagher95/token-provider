d9d78c489dd81634d4d2a5ca3f574e6e93cc4798
Sam <gallaghersam95@gmail.com>
2020-11-09 11:47:05 -0600

    added type module

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,1 @@
-declare module "token-provider" {}
+export {};
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { RedisStorageProvider } from "./Storage/redis";
+import { Token } from "./Token";
+import { TokenProvider } from "./TokenProvider";
